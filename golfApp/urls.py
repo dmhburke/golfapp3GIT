afba0761c4d1ca01cd879abadba2ddb38cef73ba
Daniel Burke <dmhburke@Daniels-Air.nyc.rr.com>
2019-10-13 00:11:42 -0400

    reverse previous mapping

--- golfApp/urls.py
+++ golfApp/urls.py
@@ -22,6 +22,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('', include('catalog.urls')),
+    path('', RedirectView.as_view(url='', permanent=True)),
 ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

67d303ddae25294ed46499caec61e4489d30eef4
Daniel Burke <dmhburke@Daniels-Air.nyc.rr.com>
2019-10-12 23:27:29 -0400

    Added files and changes required for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn golfApp.wsgi --log-file -
--- golfApp/settings.py
+++ golfApp/settings.py
@@ -151,3 +151,8 @@ STATICFILES_FINDERS = (
 )
 AWS_DEFAULT_ACL = None
 AWS_PRELOAD_METADATA=True
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
